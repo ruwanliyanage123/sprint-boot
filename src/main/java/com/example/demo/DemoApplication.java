ef490ff119cc2f0c955cf0737445c68a250f6878
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-10-13 15:06:01 +0530

    Add mothod to hello world

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+    @RequestMapping(value = "/hello") public String hello() {
+        return "Hello world";
+    }
 }
